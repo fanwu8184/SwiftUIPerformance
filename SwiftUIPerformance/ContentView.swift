473c30ce8a5ec3bc65c60059bdbed83727b549c6
fan-wu <fan.wu@rea-group.com>
2025-03-07 14:14:19 +1100

    Update reference

--- SwiftUIPerformance/ContentView.swift
+++ SwiftUIPerformance/ContentView.swift
@@ -1,11 +1,5 @@
 import SwiftUI
 
-// References:
-// Youtuber: Karin Prater
-// SwiftUI Performance Optimisation: How to manage Data Flow and UI Updates in your iOS and macOS apps https://www.youtube.com/watch?v=yvfv6N60-vY
-// The Essentials of SwiftUI View Identity: How to Ensure Reliable View Updates Across Your iOS Apps https://www.youtube.com/watch?v=tRp6I3epizk
-// From Slow to Fast: Profiling SwiftUI Applications for Peak Performance - SwiftUI Testing Tutorial 1 https://www.youtube.com/watch?v=aH15GUzk85Y
-
 struct ContentView: View {
     var body: some View {
         TabView {
